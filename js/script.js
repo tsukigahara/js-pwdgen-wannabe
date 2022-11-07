dadc6b631c4eb11eb5a71eb38c5b4d4dfff87b22
tsukigahara <satsuki.tksm@gmail.com>
2022-11-07 09:53:38 +0100

    master

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var name = window.prompt("what is your name?");
+var surname = window.prompt("what is your surname?");
+var favcolor = window.prompt("what is your favorite color?");
+document.write("your password is "+name+surname+favcolor+"</br> bye!")
+
+
+
